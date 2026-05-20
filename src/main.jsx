c8f359e4caa7422541f65f793c78aaf9bb0364b0
andrei12388 <robertandreib.up@gmail.com>
2026-05-20 15:50:26 +0800

    added attendance temp

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { NotificationProvider } from './components/notificationToast.jsx'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import VineAttendancePage from './pages/VineAttendancePage'
 
 createRoot(document.getElementById('root')).render(
